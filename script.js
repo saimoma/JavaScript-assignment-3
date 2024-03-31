80ce870b952e8b15de23f9f6a2a65a4fc013ee6b
saimomaaktersakila <saimomaakter@gmail.com>
2024-03-31 21:16:03 +0600

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
-const sakila = ["saimoma", "mim", "atiya"]
 
-console.log(sakila)
+{
+    const sakila=['saimoma','zahan','sakila'];
+    console.log(sakila)
+}
+
+
+{
+    const sinthia={
+        name:"Saimoma",
+        age:17,
+        group:"Web Development",
+        location:"Raidakshin"
+
+    }
+    console.log(sinthia)
+}
+
+
+{
+    const saimoma=['Raidakshin','Joymontop','Singair'];
+    saimoma.push('Manikganj')
+    console.log(saimoma)  
+}
+
+
+{
+    var zahan=['Bangladesh','Dhaka','Manikganj','Singair'];
+    zahan.pop("Raidakshin");
+    console.log(zahan)  
+}
+
+
+
+{
+    const jannat=['atia','sakila','mariya','sumi'];
+    console.log(jannat.length)
+}
+
+
+
+
+let money = 70000 ;
+
+if(money >= 80000){
+    console.log("Buy MAC");
+}
+else if ( money >= 60000 && money>80000 ){
+    console.log("Buy Gaming Laptop");
+}
+ else if ( money >=40000 && money < 60000 ){
+    console.log("Buy Lenovo Yoga");
+ }
+ else if( money >= 20000 && money<40000 ){
+    console.log("Buy Secondhand Laptop");
+ }
+ else{
+    console.log("Go with Mobile");
+ }
+
+
